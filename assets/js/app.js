8f256e0c6c9e853180f4551c4b6c849f6e35b19e
Alyssa Manse <alyssa.manse@austin.utexas.edu>
2017-10-05 10:03:45 -0500

    init commit

---
+++ assets/js/app.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+	// Initialize parallax feature in Materialize
+    $('.parallax').parallax();
+
+    // Initialize slider feature in Materialize
+  	$('.slider').slider();
+
+  	// Initialize carousel feature in Materialize
+  	$('.carousel.carousel-slider').carousel({fullWidth: true});
+
+});
