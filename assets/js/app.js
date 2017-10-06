aa4e66f5389b78a9e980f4b6f756bcc2958ba753
Alyssa Manse <alyssa.manse@austin.utexas.edu>
2017-10-05 22:04:43 -0500

    added smooth scroll

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,4 +9,16 @@ $(document).ready(function(){
   	// Initialize carousel feature in Materialize
   	$('.carousel.carousel-slider').carousel({fullWidth: true});
 
+  	$('a[href^="#"]').on('click', function(event) {
+
+    var target = $(this.getAttribute('href'));
+    
+    if( target.length ) {
+        event.preventDefault();
+        $('html, body').stop().animate({
+            scrollTop: target.offset().top
+        }, 1000);
+    }
+});
+
 });
