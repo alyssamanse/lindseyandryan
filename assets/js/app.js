8b03945108b04e5a8d4a57ea957ebb60cdb46fb2
Alyssa Manse <alyssa.manse@austin.utexas.edu>
2017-11-05 00:16:04 -0500

    added scroll to top btn

--- assets/js/app.js
+++ assets/js/app.js
@@ -21,6 +21,20 @@ $(document).ready(function(){
 	    }
 	});
 
+	$(window).scroll(function(){
+		if ($(this).scrollTop() > 100) {
+			$('.scrollToTop').fadeIn();
+		} else {
+			$('.scrollToTop').fadeOut();
+		}
+	});
+	
+	//Click event to scroll to top
+	$('.scrollToTop').click(function(){
+		$('html, body').animate({scrollTop : 0},800);
+		return false;
+	});
+
 	$(".your-clock").TimeCircles(
 		{ time: {
 		    Days: { color: "#f0d1c3" },
